5e9cb7882dd1a5bc8bbca1c05ca9694bafb89389
thimovandermeer <thimovandermeer@gmail.com>
2025-09-27 13:38:28 +0200

    Move visualization in sep class

---
+++ Vizualizer/Visualizer.hpp
@@ -0,0 +1,101 @@
+#pragma once
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <optional>
+#include <thread>
+#include <chrono>
+#include "QuotesObtainer.hpp"
+#include "websocket/BitVavoNetworkClient.hpp"
+
+namespace demo {
+
+class Visualizer {
+public:
+    Visualizer(gateway::QuotesObtainer<gateway::BitvavoWebSocketClient>& obt,
+               std::string market)
+        : obt_(obt), market_(std::move(market)) {}
+
+    void run() {
+        using namespace std::chrono;
+
+        print_header();
+
+        auto lastPrint = steady_clock::now();
+        std::size_t bidsThisInterval = 0;
+        std::size_t asksThisInterval = 0;
+
+        gateway::Quote q{};
+        std::optional<gateway::Quote> latestBid;
+        std::optional<gateway::Quote> latestAsk;
+
+        for (;;) {
+            // Drain queues
+            while (obt_.getBidQueue().pop(q)) { latestBid = q; ++bidsThisInterval; }
+            while (obt_.getAskQueue().pop(q)) { latestAsk = q; ++asksThisInterval; }
+
+            // Print once per second
+            auto now = steady_clock::now();
+            if (now - lastPrint >= 1s) {
+                print_line(latestBid, latestAsk, bidsThisInterval, asksThisInterval);
+                bidsThisInterval = asksThisInterval = 0;
+                lastPrint = now;
+            }
+
+            std::this_thread::sleep_for(10ms);
+        }
+    }
+
+private:
+    gateway::QuotesObtainer<gateway::BitvavoWebSocketClient>& obt_;
+    std::string market_;
+
+    static void print_header() {
+        std::cout << "Time                 | Market   | Latest Bid            | Latest Ask            "
+                     "| Peak Bid             | Peak Ask             | Bids/s | Asks/s\n";
+        std::cout << "---------------------+----------+-----------------------+-----------------------"
+                     "+----------------------+----------------------+--------+-------\n";
+    }
+
+    void print_line(const std::optional<gateway::Quote>& latestBid,
+                    const std::optional<gateway::Quote>& latestAsk,
+                    std::size_t bidsThisInterval,
+                    std::size_t asksThisInterval) {
+        using namespace std::chrono;
+
+        // Timestamp
+        auto sysnow = system_clock::now();
+        auto t = system_clock::to_time_t(sysnow);
+        std::tm tm{};
+        localtime_r(&t, &tm);
+
+        std::ostringstream line;
+        line << std::put_time(&tm, "%F %T") << " | "
+             << std::setw(8) << market_ << " | ";
+
+        auto fmt = [](const std::optional<gateway::Quote>& qq) {
+            std::ostringstream os; os.setf(std::ios::fixed); os << std::setprecision(2);
+            if (qq) os << std::setw(8) << qq->getPrice() << " x " << std::setw(7) << qq->getSize();
+            else    os << std::setw(8) << "-" << " x " << std::setw(7) << "-";
+            return os.str();
+        };
+
+        line << std::setw(23) << fmt(latestBid) << " | "
+             << std::setw(23) << fmt(latestAsk) << " | ";
+
+        line << std::setw(21);
+        if (obt_.peakBidQuote_) line << std::fixed << std::setprecision(2) << obt_.peakBidQuote_->getPrice(); else line << "-";
+        line << " | ";
+
+        line << std::setw(21);
+        if (obt_.peakAskQuote_) line << std::fixed << std::setprecision(2) << obt_.peakAskQuote_->getPrice(); else line << "-";
+        line << " | ";
+
+        line << std::setw(6) << bidsThisInterval << " | "
+             << std::setw(5) << asksThisInterval;
+
+        std::cout << line.str() << '\n';
+    }
+};
+
+} // namespace demo
--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #include <iomanip>
 #include <chrono>
 #include "GatewayIn/include/QuotesObtainer.hpp"
-#include "GatewayIn/include/websocket/BitVavoNetworkClient.hpp"
+#include "Vizualizer/Visualizer.hpp"
 
 static void print_header() {
 	std::cout << "Time                 | Market   | Latest Bid            | Latest Ask            | Peak Bid             | Peak Ask             | Bids/s | Asks/s\n";
@@ -29,64 +29,7 @@ int main() {
 	}
 
 	std::cout << "Connected. Press Ctrl+C to exit.\n";
-	print_header();
 
-	// Visualization loop
-	auto lastPrint = steady_clock::now();
-	std::size_t bidsThisInterval = 0;
-	std::size_t asksThisInterval = 0;
-
-	gateway::Quote q{};
-	std::optional<gateway::Quote> latestBid;
-	std::optional<gateway::Quote> latestAsk;
-
-	for (;;) {
-		// Drain incoming quotes quickly
-		while (obt.getBidQueue().pop(q)) { latestBid = q; ++bidsThisInterval; }
-		while (obt.getAskQueue().pop(q)) { latestAsk = q; ++asksThisInterval; }
-
-		// Print every second
-		auto now = steady_clock::now();
-		if (now - lastPrint >= 1s) {
-			// Compose timestamp
-			auto sysnow = system_clock::now();
-			auto t = system_clock::to_time_t(sysnow);
-			std::tm tm{};
-
-			localtime_r(&t, &tm);
-
-			std::ostringstream line;
-			line << std::put_time(&tm, "%F %T") << " | "
-				 << std::setw(8) << market << " | ";
-
-			auto fmt = [](const std::optional<gateway::Quote>& qq) {
-				std::ostringstream os; os.setf(std::ios::fixed); os << std::setprecision(2);
-				if (qq) os << std::setw(8) << qq->getPrice() << " x " << std::setw(7) << qq->getSize();
-				else    os << std::setw(8) << "-" << " x " << std::setw(7) << "-";
-				return os.str();
-			};
-
-			line << std::setw(23) << fmt(latestBid) << " | "
-				 << std::setw(23) << fmt(latestAsk) << " | ";
-
-			line << std::setw(21);
-			if (obt.peakBidQuote_) line << std::fixed << std::setprecision(2) << obt.peakBidQuote_->getPrice(); else line << "-";
-			line << " | ";
-			line << std::setw(21);
-			if (obt.peakAskQuote_) line << std::fixed << std::setprecision(2) << obt.peakAskQuote_->getPrice(); else line << "-";
-			line << " | ";
-
-			line << std::setw(6) << bidsThisInterval << " | "
-				 << std::setw(5) << asksThisInterval;
-
-			std::cout << line.str() << '\n';
-
-			bidsThisInterval = 0;
-			asksThisInterval = 0;
-			lastPrint = now;
-		}
-
-		std::this_thread::sleep_for(10ms);
-	}
+	demo::Visualizer vis(obt, market);
+	vis.run();
 }
-
